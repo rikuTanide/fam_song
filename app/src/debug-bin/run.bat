64b897c1a6e0c0e569a3dd4cdc80bca032b6453f
riku <riku@tanide.net>
2020-11-22 15:32:43 +0900

    tsxじゃなくした

--- app/src/debug-bin/run.bat
+++ app/src/debug-bin/run.bat
@@ -1,5 +1,5 @@
-call .\node_modules\.bin\ts-node src\debug-bin\artist_page.tsx > src\debug-bin\artist_page.html
-call .\node_modules\.bin\ts-node src\debug-bin\song_page.tsx > src\debug-bin\song_page.html
-call .\node_modules\.bin\ts-node src\debug-bin\top_page.tsx > src\debug-bin\top_page.html
-call .\node_modules\.bin\ts-node src\debug-bin\user_page.tsx > src\debug-bin\user_page.html
-call .\node_modules\.bin\ts-node src\debug-bin\vote_page.tsx > src\debug-bin\vote_page.html
+call .\node_modules\.bin\ts-node src\debug-bin\artist_page.ts > src\debug-bin\artist_page.html
+call .\node_modules\.bin\ts-node src\debug-bin\song_page.ts > src\debug-bin\song_page.html
+call .\node_modules\.bin\ts-node src\debug-bin\top_page.ts > src\debug-bin\top_page.html
+call .\node_modules\.bin\ts-node src\debug-bin\user_page.ts > src\debug-bin\user_page.html
+call .\node_modules\.bin\ts-node src\debug-bin\vote_page.ts > src\debug-bin\vote_page.html
